f87fdfeffb9a568e7c4bdc63d0ed1d2ea208a94a
Pavel Belikov <pbelikov@gmail.com>
2018-12-31 19:04:34 +0300

    feat(helpers): Helpers static methods
    - moved helpers to be collection of static methods
    - fixed gender CSS
    
    Co-Authored-By: pbelikov-gd <pbelikov-gd@users.noreply.github.com>

---
+++ TODO.md
@@ -0,0 +1,4 @@
+[ ] Make `Helper` - collection of static methods, instead of singleton, that does not care about instance properties
+[ ] Implement `Zoom` capability
+[ ] Implement text-editor with pseudo-two-way data-binding
+[ ] Implement ranking (i.e. stars): we'll need some `store` to keep this data, because right now its just visual effects
--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,75 +1,46 @@
-let helper;
-
-class Helper {
+/**
+ * @class
+ * @desc Set of static methods, that are used to display all HTML and CSS (i.e. to make interactive mockup).
+ * Once we have some framework here, then this class is likely to be removed.
+ */
+class Helpers {
   constructor() {
-    let self = this;
 
-    console.log('Instantiating Helper ...');
-    self._initButtons();
-    self._initSlider();
   }
 
-  // Public methods
+  // Static methods
 
-  buy() {
-    let self = this;
+  static buy() {
     // IE 9+ solution, older ones not being supported (we'll have to make a workaround
     // with splitting className and joining it back)
     document.querySelector('.buy-modal').classList.toggle('not-displayed');
-    self._blur();
+    Helpers.blur();
   }
 
-  edit() {
-    let self = this;
+  static edit() {
     document.querySelector('.edit-modal').classList.toggle('not-displayed');
-    self._blur();
+    Helpers.blur();
   }
 
-  delete() {
-    let self = this;
+  static delete() {
     document.querySelector('.delete-modal').classList.toggle('not-displayed');
-    self._blur();
+    Helpers.blur();
   }
 
-  closeModal() {
-    let self = this;
+  static closeModal() {
     document.querySelectorAll('.modal-container').forEach((item) => {
       item.classList.add('not-displayed');
     });
-    self._blur();
+    Helpers.blur();
+  }
+
+  static blur() {
+    document.querySelector('body').classList.toggle('has-modal-active');
   }
 
   // Private methods
 
-  _blur() {
-    document.querySelector('body').classList.toggle('has-modal-active');
-  }
-
-  _initButtons() {
-    let self = this;
-
-    if (!!document.querySelector('.buy-button')) {
-      document.querySelector('.buy-button').addEventListener('click', (event) => {
-        self.buy();
-      });
-    }
-
-    if (!!document.querySelector('.edit-button')) {
-      document.querySelector('.edit-button').addEventListener('click', (event) => {
-        self.edit();
-      });
-    }
-
-    if (!!document.querySelector('.delete-button')) {
-      document.querySelector('.delete-button').addEventListener('click', (event) => {
-        self.delete();
-      });
-    }
-  }
-
-  _initSlider () {
-    let self = this;
-
+  static initSlider () {
     // Slider is made of jQuery UI slider, so binding is made this way
     if (document.querySelector('.filter-options .slider')) {
       document.querySelectorAll('.filter-options .slider').forEach(obj => {
@@ -99,12 +70,17 @@ class Helper {
 
 document.addEventListener('DOMContentLoaded', () => {
   // Do nothing, if we are already done
-  if (window.helper) {
+  if (window.Helpers) {
     return;
   }
 
   // Pulling it to global scope for now, as soon as we'll remove it once framework is here
-  window.helper = new Helper();
+  window.Helpers = Helpers;
+
+  // Now - init the slider
+  Helpers.initSlider();
 });
 
-module.exports = {Helper, helper};
+module.exports = {
+  Helpers
+};
